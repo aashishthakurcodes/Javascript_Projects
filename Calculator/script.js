d9544bd8f50e6d438594dfdff92fec688764ff5f
Aashish Thakur <j9183208@gmail.com>
2023-03-14 22:31:47 +0530

    Calculator completed

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,24 +1,22 @@
+// Function to push user data into main input
 function valuebtn(e) {
   calculator_form.screen.value += e.value;
-  console.log("hello");  
-  console.log(calculator_form.screen.value); 
+  console.log("hello");
+  console.log(calculator_form.screen.value);
 }
-
-function Clear(){
-  calculator_form.screen.value=null;
+// Function to clear the input
+function Clear() {
+  calculator_form.screen.value = null;
 }
+//Targeting the input
+let dta = document.getElementById("input_data");
 
-let dta=document.getElementById("input_data")
-
-function calculate(){
-  // console.log(dta.value);
-  if(!dta.value){
-    alert("Please enter some data")
-    calculator_form.screen.value=null;
+//Function that check the input.value id empty or not . if empty display alert msg. otherwise execute the further code
+function calculate() {
+  if (!dta.value) {
+    alert("Please enter some data");
+    calculator_form.screen.value = null;
+  } else {
+    calculator_form.screen.value = eval(calculator_form.screen.value);
   }
-  else{
-  calculator_form.screen.value=eval(calculator_form.screen.value)
-}
-  
-  
 }
