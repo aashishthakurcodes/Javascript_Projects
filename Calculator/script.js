2818788cefe3069c7d84e157b4b1ad1be964a569
Aashish Thakur <j9183208@gmail.com>
2023-03-14 22:08:26 +0530

    Calculator completed

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,12 +1,24 @@
 function valuebtn(e) {
   calculator_form.screen.value += e.value;
-  console.log(screen);   
+  console.log("hello");  
+  console.log(calculator_form.screen.value); 
 }
 
 function Clear(){
   calculator_form.screen.value=null;
 }
 
+let dta=document.getElementById("input_data")
+
 function calculate(){
+  // console.log(dta.value);
+  if(!dta.value){
+    alert("Please enter some data")
+    calculator_form.screen.value=null;
+  }
+  else{
   calculator_form.screen.value=eval(calculator_form.screen.value)
+}
+  
+  
 }
